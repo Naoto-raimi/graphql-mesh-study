997f8979c0b22d94033cf4639751d3d998f92307
naoto tanaka <7010raimi@gmail.com>
2023-09-15 11:03:40 +0900

    permission

--- author.resolvers.ts
+++ author.resolvers.ts
@@ -3,10 +3,29 @@ import { Resolvers } from './.mesh'
 const resolvers: Resolvers = {
   authors_v1_Author: {
     namePlusId: root =>
-      // root should contain full response from REST service, instead it
-      // only contains `email` since is the only non-virtual field requested in the query
       `${root.name} ${root.id}`
   }
+  // Book: {
+  //   author: {
+  //     selectionSet: /* GraphQL */`
+  //     {
+  //       authorId
+  //     }
+  //     `,
+  //     resolve: async (root, _args, context, info) => {
+  //       return await context.Authors.Query.authors_v1_AuthorsService_GetAuthor({
+  //         root,
+  //         args: {
+  //           input: {
+  //             id: root.authorId,
+  //           }
+  //         },
+  //         context,
+  //         info,
+  //       })
+  //     }
+  //   }
+  // },
 }
 
 export default resolvers
